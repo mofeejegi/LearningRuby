3f965dc374cc11983beee9971e2af50cc09b730e
Mofe Ejegi <mofeejegi@gmail.com>
2020-02-12 13:13:46 +0100

    Taking user input

---
+++ data_types.rb
@@ -0,0 +1,17 @@
+
+name = ' Mofe '
+age = 75
+gpa = 3.4999
+is_male = true
+is_tall = false
+flaws = nil
+
+puts name.upcase.downcase.strip[0, 3].index('o')
+puts 'Hello'.upcase
+
+puts 5.0 / 9
+
+puts 'my age is ' + age.to_s
+puts gpa.floor
+
+puts Math.sqrt(81)
