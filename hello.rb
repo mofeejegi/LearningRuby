ed3642cead783cc0d7821c188eba34e3d6a717d0
Mofe Ejegi <mofeejegi@gmail.com>
2020-02-12 10:48:46 +0100

    added variables

--- hello.rb
+++ hello.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-puts 'My name is Mofe'
+first_name = "Mofe"
+last_name = "Ejegi"
+
+puts('My first name is ' + first_name)
+puts 'My last name is ' + last_name
 print 'Hello World!'
 
